8243f4876721d8a61def088b842966ac21083a3e
Mayowa Pitan <andela-mpitan@users.noreply.github.com>
2016-03-05 11:07:49 +0100

    Update extra_methods.rb

--- lib/paitin_hangman/extra_methods.rb
+++ lib/paitin_hangman/extra_methods.rb
@@ -1,80 +1,80 @@
 require_relative "messages"
 
 module PaitinHangman
-module SimpleMethods
+  module SimpleMethods
+    
+    def verify_name_integrity
+      name = gets.chomp.upcase.strip
+      while name.scan(/[^A-Z\s-]/).empty? == false || name.empty?
+        Message.verify_name
+        name = STDIN.gets.chomp.upcase.strip
+      end
+      name
+    end
   
-  def verify_name_integrity
-    name = gets.chomp.upcase.strip
-    while name.scan(/[^A-Z\s-]/).empty? == false || name.empty?
-      Message.verify_name
-      name = STDIN.gets.chomp.upcase.strip
+    # => This technically begins the game
+    def play(name)
+      Message.game_type
+      option_integrity
+      if @option == "2"
+        Computer.new.level_integrity(name)
+      else
+        Player.new.first_player_name(name)
+      end
     end
-    name
-  end
-
-  # => This technically begins the game
-  def play(name)
-    Message.game_type
-    option_integrity
-    if @option == "2"
-      Computer.new.level_integrity(name)
-    else
-      Player.new.first_player_name(name)
-    end
-  end
-  # => The method makes sure a valid option is picked
-
-  def option_integrity
-    @option = gets.chomp
-    until @option == "1" || @option == "2"
-      puts "Enter either a '1' or a '2'"
-      @option = STDIN.gets.chomp
-    end
-    @option
-  end
-
-  def length_one?(parameter)
-    if parameter.length == 1
-      return true
-    else
-      puts "Enter just one letter or an hyphen or an apostrophe"
-      return false
-    end
-  end
-
-  def unique?(parameter)
-    if @misses.include?(parameter) || @right_guesses.include?(parameter)
-      puts "You have used #{parameter} already, try again"
-      return false
-    else
-      return true
-    end
-  end
-
-  def number?(parameter)
-    if parameter.scan(/\d/).empty?
-      return true
-    else
-      puts "You cannot enter a number! Try again"
-      return false
-    end
-  end
+    # => The method makes sure a valid option is picked
   
-  def choice_integrity
-    choice = gets.chomp.downcase
-    until choice == "r" || choice == "q" || choice == "quit"
-      puts "You must enter either a 'r' or 'q' or type 'quit'"
-      choice = STDIN.gets.chomp.downcase
+    def option_integrity
+      @option = gets.chomp
+      until @option == "1" || @option == "2"
+        puts "Enter either a '1' or a '2'"
+        @option = STDIN.gets.chomp
+      end
+      @option
     end
-    decide(choice)
-  end
-
-  def decide(choice)
-    if choice == "r"
-      @player1 ? play(@player1) : play(@player2)
-    else
-      exit
+  
+    def length_one?(parameter)
+      if parameter.length == 1
+        return true
+      else
+        puts "Enter just one letter or an hyphen or an apostrophe"
+        return false
+      end
+    end
+  
+    def unique?(parameter)
+      if @misses.include?(parameter) || @right_guesses.include?(parameter)
+        puts "You have used #{parameter} already, try again"
+        return false
+      else
+        return true
+      end
+    end
+  
+    def number?(parameter)
+      if parameter.scan(/\d/).empty?
+        return true
+      else
+        puts "You cannot enter a number! Try again"
+        return false
+      end
+    end
+    
+    def choice_integrity
+      choice = gets.chomp.downcase
+      until choice == "r" || choice == "q" || choice == "quit"
+        puts "You must enter either a 'r' or 'q' or type 'quit'"
+        choice = STDIN.gets.chomp.downcase
+      end
+      decide(choice)
+    end
+  
+    def decide(choice)
+      if choice == "r"
+        @player1 ? play(@player1) : play(@player2)
+      else
+        exit
+      end
     end
   end
-end
 end
