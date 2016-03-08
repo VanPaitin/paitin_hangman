84abc5cfc5c51cbdc3d3fccf8ca200419eafae26
Mayowa Pitan <mayowa.pitan@andela.com>
2016-03-08 11:39:35 +0100

    Updated my specs

--- spec/game_resumption_spec.rb
+++ spec/game_resumption_spec.rb
@@ -45,13 +45,53 @@ describe PaitinHangman::GameResumption do
       expect(@instance.choice_integrity).to eql "properties"
     end
   end
+  # describe '#load_properties' do
+    
+  #   before :each do
+  #     @dummy = PaitinHangman::GameData.new("player_name", ['m', 'i', 's', 's', 'e', 's'], %w{r i g h t g u e s s e s},
+  #                                            "chances", "word_control", "gameword", "count")
+  #   end
+  #   it "sets the game properties" do
+  #     @instance.instance_variable_set("@game_object", @dummy)
+  #     allow(@instance).to receive(:load_properties_cont).and_return "trials"
+  #     expect(@instance.load_properties).to eql "trials"
+  #   end
+  # end
+
+  # describe '#load_properties_cont' do
+  #   before do
+  #     file_name = File.join(File.dirname(File.expand_path(__FILE__)), '../../games.yml')
+  #     @all_games = YAML.load_stream(File.open(file_name, "a+"))
+  #       @saved_game = @all_games.select do |game|
+  #       game.player_name == name
+  #     end
+  #   end
+  #   it "sets the property of count and return trials" do
+  #     allow(@instance).to receive(:trials).and_return("game")
+  #     expect(@instance.load_properties_cont).to eql "game"
+  #   end
+  # end
+
+  describe '#print_function' do
+  end
+
   describe '#trials' do
-    it 'can run a resmed game successfully' do
+    it 'can run a resumed game successfully' do
+      @instance.instance_variable_set("@word_control", "_i__i_i__")
       allow(@instance).to receive(:verify_guess).and_return(nil)
       allow(@instance).to receive(:compare_guess).and_return(nil)
       allow(@instance).to receive(:win_game).and_return(nil)
       allow(@instance).to receive(:end_game).and_return(nil)
       expect(@instance.trials(10)).to eql nil
+    end
+  end
+
+  describe '#setup' do
+    context "it can print the game status to the screen" do
+      before do
+        @instance.instance_variable_set("@word_control", "_i__i_i__")
+      end
+      specify { expect { @instance.setup }.to output("Now, continue playing...\n__ I__ __ I__ I__ __ \n").to_stdout }
     end
   end
 end
